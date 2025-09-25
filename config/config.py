3f07e6a2a844196e94a8e9a3ccd8cfa2dfc183c1
machepur <machepur@ncsu.edu>
2025-09-24 21:59:33 -0400

    [Implementation] Finished methods in file_utils.py

--- config/config.py
+++ config/config.py
@@ -1,1 +1,25 @@
 # Read API keys from .env, chunk sizes, model settings
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Configure the APIs
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+VOYAGEAI_API_KEY = os.getenv("VOYAGEAI_API_KEY")
+
+# Chunking Configuration
+CHUNK_SIZE = 500
+CHUNK_OVERLAP = 50
+
+# Search Configuration
+TOP_K_RESULTS = 3
+EMBEDDING_MODEL = "voyage-3-large"
+CLAUDE_MODEL = "claude-3-sonnet-20240229"
+
+# File Paths
+DOCUMENTS_DIR = "documents"
+CHUNKS_DIR = "data/chunks"
+EMBEDDINGS_DIR = "data/embeddings"
+INDEXES_DIR = "data/indexes"
--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,1 +1,64 @@
-# File handling helpers
+# File handling helper methods
+import os
+import json
+from pathlib import Path
+from pypdf import PdfReader
+from docx import Document
+from typing import Dict
+
+def ensure_directory_exists(directory_path):
+    """Create directory if it doesn't exist"""
+    Path(directory_path).mkdir(parents=True, exist_ok=True)
+
+def load_pdf(file_path: str) -> str:
+    """Extract the text from a PDF file"""
+    # Use PdfReader to get the text
+    reader = PdfReader(file_path)
+    text = []
+    for page in reader.pages:
+        text.append(page.extract_text() or "")
+    text = "\n".join(text)
+    # Return all the text from the pdf
+    return text
+
+def load_txt(file_path: str) -> str:
+    """Load text from .txt file"""
+    # Use basic file IO to get text from the .txt file
+    with open(file_path, "r", encoding="utf-8") as f:
+        text = f.read()
+    # Return the text as a str
+    return text
+
+def load_docx(file_path: str) -> str:
+    """Extract text from Word document"""
+    # Using python-docx library get all the text
+    doc = Document(file_path)
+    text = "\n".join([para.text for para in doc.paragraphs])
+    # Return the text
+    return text
+
+def save_json(text: str, file_path: str) -> None:
+    """Save data to JSON file"""
+    # Turn all text to a dict
+    text_data = {
+        "text": text
+    }
+    # Use json library to write text to a .json file
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(text_data, f, indent=4)
+
+def load_json(file_path: str) -> Dict:
+    """Load data from JSON file"""
+    # Load the text from the json file as a json object
+    with open(file_path, "r", encoding="utf-8") as f:
+        text_data = json.load(f)
+    # Return the json object
+    return text_data
+
+
+def main():
+    pdf_text = load_pdf("documents/Manish_Chepuri_Resume.pdf")
+    save_json(pdf_text, "data/data.json")
+    print(type(load_json("data/data.json")))
+    
+main()
